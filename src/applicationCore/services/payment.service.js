9c9d838b7c9637c10e8b7a390177259ddf96aee3
Tuan Nguyen <ntuan.2502@gmail.com>
2022-06-15 13:41:49 +0700

    fix payment api

--- src/applicationCore/services/payment.service.js
+++ src/applicationCore/services/payment.service.js
@@ -1,27 +1,39 @@
 const Payment = require('../../applicationData/entities/payment');
 const ServiceResult = require('../common/serviceResult');
-const { CREATE, READ_MANY } = require('../common/applicationConstant');
+const { CREATE, READ_MANY, NOT_FOUND, READ_ONE } = require('../common/applicationConstant');
 const { loggingEvent } = require('../common/myUltility');
 
 module.exports = class PaymentService {
-    async create(user, books, paymentName, total) {
+    async create(userId, books, paymentName, total) {
         const payment = new Payment({
-            user,
+            userId,
             books,
             paymentName,
             total,
         });
         await payment.save();
-        loggingEvent(payment, 'CREATE', 'PAYMENT', true, payment._id, user, '', payment);
+        loggingEvent(payment, 'CREATE', 'PAYMENT', true, payment._id, userId, '', payment);
 
         return new ServiceResult(true, CREATE, { payment });
     }
 
-    async findAll() {
-        const payments = await Payment.find({});
+    async findAll(userId) {
+        const payments = await Payment.find({ userId });
 
         loggingEvent(payments, 'READ_MANY', 'PAYMENT', true, null, null, '', null);
 
         return new ServiceResult(true, READ_MANY, payments);
+    }
+
+    async find(userId, bookId) {
+        const payment = await Payment.findOne({ userId, bookId });
+        if (!payment) {
+            loggingEvent(payment, 'READ_ONE', 'PAYMENT', false, null, null, '404', null);
+            return new ServiceResult(false, NOT_FOUND);
+        }
+
+        loggingEvent(payment, 'READ_ONE', 'PAYMENT', true, null, null, '', null);
+
+        return new ServiceResult(true, READ_ONE, payment);
     }
 };
--- src/webAPI/controllers/payment.controller.js
+++ src/webAPI/controllers/payment.controller.js
@@ -4,19 +4,28 @@ class PaymentController {
     // [POST] /payment/
     async create(req, res) {
         const paymentService = new PaymentService();
-        const user = req.user._id;
+        const userId = req.user._id;
         const books = req.body.books;
         const paymentName = req.body.paymentName;
         const total = req.body.total;
 
-        return res.send(await paymentService.create(user, books, paymentName, total));
+        return res.send(await paymentService.create(userId, books, paymentName, total));
     }
 
     // [GET] /payment/
     async findAll(req, res) {
         const paymentService = new PaymentService();
+        const userId = req.user._id;
 
-        return res.send(await paymentService.findAll());
+        return res.send(await paymentService.findAll(userId));
+    }
+
+    // [GET] /payment/bookId
+    async find(req, res) {
+        const paymentService = new PaymentService();
+        const userId = req.user._id;
+        const bookId = req.params.bookId;
+        return res.send(await paymentService.find(userId, bookId));
     }
 }
 module.exports = new PaymentController();
